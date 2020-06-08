9b903e3b0c280803cea8e5aa667b26e5bb667cd6
gregid <gtrzeciak@gmail.com>
2020-06-08 22:00:20 +0200

    first commit

---
+++ lib-src/lunchtime.js
@@ -0,0 +1,15 @@
+export default function getNextLunchtime (hours, minutes) {
+	var lunchtime = new Date();
+
+	lunchtime.setHours(hours);
+	lunchtime.setMinutes(minutes);
+	lunchtime.setSeconds(0);
+	lunchtime.setMilliseconds(0);
+
+	console.log('This should point to lunchtime.js');
+	// if we've already had lunch today, start planning
+	// tomorrow's lunch
+	if (lunchtime < Date.now()) lunchtime.setDate(lunchtime.getDate() + 1);
+
+	return lunchtime;
+}
