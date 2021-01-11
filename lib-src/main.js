0c5a67025456db28279eeb50e42554f3e4cca896
Niklas Mischkulnig <mischnic@users.noreply.github.com>
2021-01-11 16:38:15 +0100

    Cleanup

--- lib-src/main.js
+++ lib-src/main.js
@@ -1,13 +1,7 @@
-import ms from 'ms';
-import lunchtime from './lunchtime.js';
-import millisecondsUntil from './millisecondsUntil.js';
+import ms from "ms";
 
-export default function howLongUntilLunch(hours, minutes) {
-	// lunch is at 12.30
-	if (hours === undefined) hours = 12;
-	if (minutes === undefined) minutes = 30;
-	console.log('This should point to main.js');
+export default function () {
+	console.log("from main.js:4");
 
-	var millisecondsUntilLunchTime = millisecondsUntil(lunchtime(hours, minutes));
-	return ms(millisecondsUntilLunchTime, { long: true });
+	return ms(1);
 }
